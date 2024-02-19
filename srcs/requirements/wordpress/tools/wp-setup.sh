36567d684e0ce522f874d5656c1a94ca2e6faabb
Lilian Morel <lmorel@z1r10p1.42lyon.fr>
2024-02-19 23:03:02 +0100

    db try 3

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -14,7 +14,7 @@ else
 	cd /var/www/html
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$DOMAIN_NAME/g" wp-config-sample.php
+	sed -i "s/localhost/mariadb/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 	
