b2100cf0d0c5300943cee3aaf47b9247b0323019
Lilian Morel <lmorel@z1r10p1.42lyon.fr>
2024-02-19 21:26:23 +0100

    wordpress kinda works

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -5,15 +5,15 @@ then
 	echo "wordpress already there"
 else
 
-	wget https://wordpress.org/lastest.tar.gz
-	tar xfz lastest.tar.gz
+	wget https://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
 	mv wordpress/* .
-	rm -rf lastest.tar.gz
+	rm -rf latest.tar.gz
 	rm -rf wordpress
 
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/localhost/$DOMAIN_NAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 
