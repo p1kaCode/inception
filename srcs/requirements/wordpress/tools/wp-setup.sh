31bdc3a1cdd8411e0fc177a944bbf07ba60a8fe2
Lilian Morel <lmorel@z1r10p1.42lyon.fr>
2024-02-19 23:49:34 +0100

    db revamped 4

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -11,12 +11,11 @@ else
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	cd /var/www/html
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/mariadb/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+	cp wp-config-sample.php /var/www/html/wp-config.php
 	
 fi
 
