f0396608d393887501624839be17157b223e7f7e
Lilian Morel <lmorel@z1r10p1.42lyon.fr>
2024-02-19 22:24:50 +0100

    wordpress config done ?

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -11,18 +11,19 @@ else
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
+	cd /var/www/html
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$DOMAIN_NAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 
-	cd /var/www/html
-	wp core download --allow-root
-	cd
-	mv wp-config.php /var/www/html/
-	wp core install --allow-root --url=${DOMAIN_NAME} --title=${MYSQL_DATABASE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-	wp user create --allow-root ${MYSQL_USER} ${WP_USER_EMAIL} --user_pass=${MYSQL_PASSWORD}
+	# cd /var/www/html
+	# wp core download --allow-root
+	# cd
+	# mv wp-config.php /var/www/html/
+	# wp core install --allow-root --url=${DOMAIN_NAME} --title=${MYSQL_DATABASE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+	# wp user create --allow-root ${MYSQL_USER} ${WP_USER_EMAIL} --user_pass=${MYSQL_PASSWORD}
 fi
 
 exec "$@"
